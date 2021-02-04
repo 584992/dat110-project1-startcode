a7fad8b67ed0eef7b9597362c026fc3ea1e34078
581257 <54100236+581257@users.noreply.github.com>
2021-02-04 17:32:47 +0100

    Update Sensor.java

--- dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
@@ -2,7 +2,11 @@ package no.hvl.dat110.system.controller;
 
 import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.*;
-
+/**
+ * 
+ * @author Regine
+ *
+ */
 public class Sensor extends RPCStub {
 
 	private byte RPCID = 1;
